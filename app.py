ad941e902cc9be4c266374faadbc0e1bfb390dd0
istshnabi <shamil.nabiyev@stud.h-da.de>
2022-01-06 11:45:21 +0100

    Added a new scatter plot and dropdown menu

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from dash import dcc
 from dash import html
 import plotly.express as px
 import pandas as pd
+from dash.dependencies import Input, Output
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 app = dash.Dash(__name__)
 
@@ -18,24 +22,69 @@ app = dash.Dash(__name__)
 #     "Amount": [4, 1, 2, 2, 4, 5],
 #     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 # })
-#
+
 # fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
+# df = pd.read_csv('data/db-cargo-delays-2016-deutschland.csv')
+# fig = px.bar(df, x="Zugfahrten", y="Verspaetungsminuten", color="weekofyear", barmode="group")
+
+# fig = make_subplots(rows=3, cols=1)
+#
+# fig.append_trace(go.Scatter(
+#    x = [3, 4, 5],
+#    y = [1000, 1100, 1200],
+# ), row = 1, col = 1)
+#
+# fig.append_trace(go.Scatter(
+#    x = [2, 3, 4],
+#    y = [100, 110, 120],
+# ), row = 2, col = 1)
+#
+# fig.append_trace(go.Scatter(
+#    x = [0, 1, 2],
+#    y = [10, 11, 12]
+# ), row = 3, col = 1)
+#
+# fig.update_layout(height = 600, width = 600, title_text = "Stacked Subplots")
+
 df = pd.read_csv('data/db-cargo-delays-2016-deutschland.csv')
-fig = px.bar(df, x="Zugfahrten", y="Verspaetungsminuten", color="weekofyear", barmode="group")
+
+columns = ['Zugfahrten', 'Verspaetungsminuten', 'month']
+grouped = df[columns].groupby(['month'])
+months = grouped.groups.keys()
+
+fig = px.scatter(data_frame=grouped.get_group(1), x='Zugfahrten', y='Verspaetungsminuten')
+
+options = list(map(lambda x:  {'label': x, 'value': x}, grouped.groups.keys()))
 
 app.layout = html.Div(children=[
-    html.H1('Hello Dash', style={'textAlign': 'center', 'color': '#7FDBFF'}),
+    html.H1('Homepage', style={'textAlign': 'center'}),
 
-    html.Div(children='''
-        Plotly Dash - First Live Demo 2
-    '''),
+    html.Div(children='Zugfahrten vs Verspaetungsminuten'),
+
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=options,
+        value=1
+    ),
+    html.Div(id='dd-output-container'),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
     )
-])
+   ],
+    style={"width": "50%"},
+)
+
+
+@app.callback(
+    Output('dd-output-container', 'children'),
+    Input('demo-dropdown', 'value')
+)
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
 
 if __name__ == '__main__':
     print('Starting app server')
